f6e4c9bfa2e08750bfd62ec7fb6815c94803b094
Spencer Hudson <spencerbrucehudson@gmail.com>
2016-10-27 12:56:01 -0600

    should be complete?

--- lesson_1.R
+++ lesson_1.R
@@ -31,7 +31,6 @@ x
 row(geno)
 # extract the diagonal of a matrix
 y <- geno[row(geno) == col(geno)]
-y
 
 # cut - divides a particular range of a vector into intervals, potentially factors
 cut(geno$Aa, breaks = 2)
@@ -58,66 +57,91 @@ ex = expand.grid(geno$AA, geno$Aa)
 x = matrix(data=rnorm(4), nrow=2, ncol=2)
 eigen(x)
 
-
 # %*% - multiples 2 matrices, granted they are conformable
 x = matrix(data=rnorm(4), nrow=2, ncol=2)
 y = matrix(data=rnorm(4), nrow=2, ncol=2)
 
 x %*% y
 
-# lower.tri 
-x = matrix(data=rnorm(4), nrow=5, ncol=5)
+# lower.tri - Returns a matrix of logicals the same size of a 
+# given matrix with entries TRUE in the lower triangle.
+x = matrix(data=rnorm(5), nrow=5, ncol=5)
 lower.tri(x)
 
-# upper.tri
+# upper.tri - Returns a matrix of logicals the same size of a 
+# given matrix with entries TRUE in the lower triangle.
 upper.tri(geno)
 
-# diag 
+# diag - Either extract/replace/construct matrix diagonal
+x = matrix(data=rnorm(5), nrow=5, ncol=5)
+y = diag(x)
 
-# gl 
+# gl - Can create labeled factors by specifying numbers of levels and replications
+gl(2, 8, labels = c("Control", "Treat"))
 
-# identical
+# identical - Tests if 2 objects demonstrate exact equality
+x = matrix(data=rnorm(5), nrow=5, ncol=5) 
+y = matrix(data=rnorm(5), nrow=5, ncol=5)
+## Expected to be FALSE, since both are matrices with random numbers
+identical(x,y, num.eq = TRUE, single.NA = TRUE, attrib.as.set = TRUE,
+          ignore.bytecode = TRUE, ignore.environment = FALSE)
 
-# image
+x = matrix(data=rnorm(5), nrow=5, ncol=5)
+x = y 
+## Expected to be TRUE since x = y
+identical(x,y, num.eq = TRUE, single.NA = TRUE, attrib.as.set = TRUE,
+          ignore.bytecode = TRUE, ignore.environment = FALSE)
 
-# library
+# image - Creates crazy grids of colored/gray-scale rectangles that correspond to a matrix. 
+## In other words, can display 3-d or spatial data 
+image(x) # I made a national flag of an unknown country
+image(y) # Rival country flag
 
-# length
+# library - synonomous with require; loads/attaches additional packages
+library(ggplot2) ## Loading an awesome plot package
 
-# jitter
+# length - Retrieves the length of a particular vector,list, factor or particular object depending on desired output
+length(geno$AA) # Length of my AA vector
 
-ls 
+# jitter - Adds a bit of noise to numerical values within a given vector
+jitter(geno$AA) # Why in the world would we use this? I have no clue
 
-what does rm(list=ls()) do?
-                                        
-mean 
+# ls - Returns vector names of objects in the form of character strings; synonomous with objects
+ls(geno)
+objects(geno)
 
-median 
+# rm(list=ls()) - removes specified loaded objects, but removes all by default
+rm(list=ls()) # where did all my objects go!?!     
 
-max
+# mean - Calculates an arithmetic average, if an object is provided, a vector must be specified
+## This will create a mean of all observations (rows) within that vector (column)
+mean(geno$AA)
 
-min
-                                        
-paste
-                                        
-read.csv 
+# median - Calculates the sample median for a data set
+## ## This will create a median for all observations (rows) within that vector (column)
+median(geno$AA)
 
-read.table 
+# max & min - Returns maxima and minima for samples in a data set
+max(geno$AA)  
+min(geno$AA)
 
-write.csv
+# paste - Concatenate vector ouputs as character strings
+paste(geno$AA, 'Dominant Homozygous Frequency')
+                             
+# read.csv - Reads in a table and formats it as a data frame; pertains to .csv files
+geno = read.csv('genotypes.csv')
 
-write.table
+# read.table - Reads in a table and formats it as a data frame
+table = read.table('example.txt')
+
+# write.csv - prints out object as data frame and saves it as a .csv file
+write.csv(geno, file = 'genotypes.csv')
+
+# write.table - prints out object as data frame and saves it as desired file format
+write.table(geno, file='genotypes.txt', quote=F, sep=' ', row.names=F, col.names=T)
 
 ################################################
 ## Bonus exercises #############################
 ################################################
 
-bonus.text <- "It was the best of times, it was the worst of times, it was the age of
-wisdom, it was the age of foolishness, it was the epoch of belief, it
-was the epoch of incredulity, it it was the season of Light, it was the
-season of Darkness, it was the spring of hope, it was the winter of
-despair, we had everything before us, we had nothing before us, we
-were all going direct to Heaven, we were all going direct the other
-way- in short, the period was so far like the present period, that
-some of its noiosiest authorities insisted on its being received, for
-good or for evil, in the superlative degree of comparison only."
+my.first.abstract = 'Bright plumage coloration is seemingly favored by females of avian species with regards to sexual selection. This particular secondary sexual characteristic has been previously tested and supported to be an honest signal of individual quality among passerines with pigment-based coloration (i.e. yellows and reds). In contrast structural plumage coloration (i.e. blues) exhibited by birds such as Indigo Buntings (Passerina cyanea) have received minimal research on relationships between plumage color intensity and aspects of physical quality physiological function. Using free-living Indigo Buntings as a study species, we compared UV and blue color intensity to innate immune responses, antioxidant capacity, stress physiology, reproductive physiology, and parasitism. UV or blue coloration of the tail feathers was not correlated with any of the physiological metrics. The overall percentage of blue feathers on individual birds was positively correlated with testosterone levels and body condition, while negatively correlated with heterophil to lymphocyte ratio. Birds with less blue coloration were more likely to harbor parasites. Our results indicate the overall blueness of male Indigo Buntings is associated with better body condition and lower stress.'
--- lesson_3.R
+++ lesson_3.R
@@ -1,9 +1,4 @@
-dexter = list(length=60, weight=40, breed='mongrel')
-class(dexter) <- 'dog'
-
-
-# 
-# print.dog = function(x, ...)
+ # print.dog = function(x, ...)
 #   cat("Woof! I can't actually talk!")
 # 
 # askdog = function(x, ...)
@@ -17,22 +12,20 @@ class(dexter) <- 'dog'
 # 
 # askdog.dog = function(x, ...)
 #   cat("Woof! A dog!")
-
 # new.dog <- function(weight, breed){
 #   output <- list(weight=weight, breed=breed)
 #   class(output) <- "dog"
 #   return(output)
 # }
 
-
 # 1. Implement a cat class, complete with race and print methods.
-noodles <- list(length=45, weight=10, breed="tabby")
-class(noodles) <- "cat"
+noodles <- list(length=45, weight=10, breed="tabby", slots = c(x=15, y=40))
+class(noodles) <- c("cat", "mammal", "point")
 class(noodles)
 noodles
 
-sawyer <- list(length=51, weight=15, breed="tabby")
-class(sawyer) <- "cat"
+sawyer <- list(length=51, weight=15, breed="tabby", x=3, y=15)
+class(sawyer) <- c("cat", "mammal")
 class(sawyer)
 sawyer
 
@@ -51,9 +44,22 @@ race(noodles, sawyer)
 
 # 2. Implement a point class that holds x and y information for a point in space.
 
+point <- setClass("point", slots = c(x=12, y=14))
+point(sawyer)
 
 
 # 3. Write a distance method that calculates the distance between two points in space.
+  distance <- function(first, second){
+    if(!inherits(first, "point") | !inherits(second, "point"))
+      stop("You haven’t given me two points!")
+    if(first$point < second$point){
+      print("First cat won!")
+      return(first)
+    }
+    print("Second cat won!")
+    return(second)
+  }
+
 # 4. Implement a line class that takes two point objects and makes a line between them.
 
 
