2579aa98ef69c43c45bbe817b8eaaf15462328fe
Spencer Hudson <spencerbrucehudson@gmail.com>
2016-11-02 15:28:47 -0600

    done through 3

--- lesson_3.R
+++ lesson_3.R
@@ -19,12 +19,12 @@
 # }
 
 # 1. Implement a cat class, complete with race and print methods.
-noodles <- list(length=45, weight=10, breed="tabby", slots = c(x=15, y=40))
-class(noodles) <- c("cat", "mammal", "point")
+noodles <- list(length=45, weight=10, breed="tabby")
+class(noodles) <- c("cat")
 class(noodles)
 noodles
 
-sawyer <- list(length=51, weight=15, breed="tabby", x=3, y=15)
+sawyer <- list(length=51, weight=15, breed="tabby")
 class(sawyer) <- c("cat", "mammal")
 class(sawyer)
 sawyer
@@ -43,23 +43,33 @@ race <- function(first, second){
 race(noodles, sawyer)
 
 # 2. Implement a point class that holds x and y information for a point in space.
-
-point <- setClass("point", slots = c(x=12, y=14))
-point(sawyer)
-
+wilmington <- list(lat=39.7391, lon=75.5398, name='Wilmington')
+class(wilmington) <- 'Point'
+logan <- list(lat=41.7, lon=111.8, name='Logan')
+class(logan) <- 'Point'
 
 # 3. Write a distance method that calculates the distance between two points in space.
-  distance <- function(first, second){
-    if(!inherits(first, "point") | !inherits(second, "point"))
-      stop("You haven’t given me two points!")
-    if(first$point < second$point){
-      print("First cat won!")
-      return(first)
-    }
-    print("Second cat won!")
-    return(second)
-  }
+deg2rad <- function(deg) {(deg*pi)/(180)}
 
-# 4. Implement a line class that takes two point objects and makes a line between them.
+  distance <- function(loc1, loc2){
+    if(!inherits(loc1, "Point") | !inherits(loc2, "Point"))
+      stop("You haven’t given me two points!")
+    R = 3961 # miles
+    lat1 = deg2rad(loc1$lat)
+    lat2 = deg2rad(loc2$lat)
+    lon1 = deg2rad(loc1$lon)
+    lon2 = deg2rad(loc2$lon)
+    dlon = lon2 - lon1
+    dlat = lat2 - lat2
+    # dlon = deg2rad(loc2$lon - loc1$lon)
+    # dlat = deg2rad(loc2$lat - loc1$lat)
+    a = (sin(dlat/2))^2 + cos(lat1) * cos(lat2) * (sin(dlon/2))^2 
+    c = 2 * atan2(sqrt(a), sqrt(1-a) ) 
+    d = R * c 
+      print(paste('The distance between', loc1$name, 'and', loc2$name, 'is approximately', round(d,digits=0), 'miles.'))
+    }
+distance(wilmington, logan)
+  
+4. Implement a line class that takes two point objects and makes a line between them.
 
 
