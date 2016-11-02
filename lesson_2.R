85bb1acc43ecfcc1cf1acb6f0d5bc4bd22d0ada2
Spencer Hudson <spencerbrucehudson@gmail.com>
2016-11-02 11:04:24 -0600

    lesson2update

--- lesson_2.R
+++ lesson_2.R
@@ -73,34 +73,37 @@ gz <- function(t, a, b, c){
 }
 gz(1:15, 10, .4, .5)
 
-
 # 8. You are beginning to suspect the biologist is taking advantage of you. Modify your function to plot in
 # purple any y value that’s above a and b. Hint: try putting 3==3 & 2==2 and 3==4 | 2==2 into an if
 # statement and see what you get. Using this construction may make this simpler.
 
-
-
+gz <- function(t, a, b, c){
+  y <- (a*exp(-b*exp(-c*t)))
+  p <- plot(y~t, xlab = 'Time (Years)', ylab= 'Population Size', type='l', col= ifelse(y > a & y > b, 'purple','red'))
+  return(paste(y,t))
+}
+gz(1:15, 10, .4, .5)
 
 # 9. Write a function that draws boxes of a specified width and height that look like this (height 3, width 5):
 #   *****
 #   *   *
 #   *****
 
-box <- function(y, x){
+box1 <- function(y, x){
   w1 <- (rep("*", x))
-  cat(w1, '\n')
+  cat(w1, '\n', sep='')
   cnt <- 0
   repeat{
-  cat('*',(rep(' ',x-2)),'*','\n')
+  cat('*',(rep('',x-3)),'*','\n')
   cnt <- cnt+1
   if(cnt > y-3){
     break
   }
 }
   w2 <- (rep('*',x))
-  cat(w2)
+  cat(w2, sep='')
 }
-box(3,5)
+box1(3,5)
 
 #   10. Modify your box function to put text centered inside the box, like this:
 #   *************
@@ -109,39 +112,39 @@ box(3,5)
 #   *           *
 #   *************
 
-box <- function(y, x, z){
+box2 <- function(y, x, z){
   z <- z
   a <- nchar(z)
-  f <- round(((x-1-(a/2))/2), digits = 0)
+  f <- round(((x-a-4))/2, digits = 0)
   w1 <- (rep("*", x))
-  cat(w1, '\n')
+  cat(w1, '\n', sep='')
   cnt <- 0
   repeat{
-    cat('*',(rep(' ',x-2)),'*','\n')
+    cat('*',(rep('',x-3)),'*','\n')
     cnt <- cnt+1
     if(cnt > y-3){
       break
     }
   }
-  cat('*',rep(' ', f-1), z, rep(' ', f-1), "*",'\n')
+  cat('*',rep('', f), z, rep('', f), "*",'\n')
     cnt <- 0
   repeat{
-    cat('*',(rep(' ',x-2)),'*','\n')
+    cat('*',(rep('',x-3)),'*','\n')
     cnt <- cnt+1
     if(cnt > y-3){
       break
     }
   }
   w2 <- (rep('*',x))
-  cat(w2)
+  cat(w2, sep='')
 }
-box(3,8,'some text')
+box2(3,15,'some text')
 
 
 #   11. Modify your box function to build boxes of arbitrary text, taking dimensions specified in terms of
 # dimensions, not the text. For example, box("t", 3, 9, "hey") might produce:
 
-box <- function(t, y, x, z){
+box3 <- function(t, y, x, z){
   z <- z
   a <- nchar(z)
   f <- round(((x-1-(a/2))/2), digits = 0)
@@ -167,4 +170,101 @@ box <- function(t, y, x, z){
   w2 <- (rep(t,x))
   cat(w2, sep='')
 }
-box('wdp',3,8,'some text')
+box3('wdp',3,8,'some text')
+
+# 12. In ecology, hurdle models are often used to model the abundance of species found on surveys. They
+# first model the probability that a species will be present at a site (drawn, for example, from a Bernoulli
+# distribution) and then model the abundance for any species that is present (drawn, for example, from
+# probability of presence (p) and that its abundance is drawn from a Poisson with a given λ. Hint: there is
+# no Bernoulli distribution in R, but the Bernoulli is a special case of what distribution?...
+
+# bernoulli, binomial, same thing right?
+# p = probability for binomial
+# n = # of random values to output
+# lam = Given parameter for Poisson distribution
+hurdle <- function(p, n, lam){
+  if(rbinom(1, 1, p) == 1){ # species presence based on binomial with given probability of success
+    ran <- rpois(n, lam) # number of sites and rate parameter
+    cat(ran)
+  }else{
+    cat('Species is Absent', 0)
+  }
+}
+
+# (presence probability, # poisson values, poisson lambda)
+hurdle(1, 1, 10)
+
+# 13. An ecologist really likes your hurdle function (will you never learn?). Write them a function that simulates
+# lots of species (each with their own p and λ) across n sites. Return the results in a matrix where each
+# species is a column, and each site a row (this is the standard used for ecology data in R).
+
+hurdle <- function(n, spnum){
+  datalist=list()
+  for(i in 1:spnum){
+    p <- rbinom(1, 1, sample(seq(from = 0, to = 1, by = .001), size = spnum, replace = TRUE))
+    # if(rbinom(1, 1, sample(seq(from = 0, to = 1, by = .001), size = spnum, replace = TRUE)) == 1){ # species presence based on binomial with given probability of success
+    # abundance <- rpois(n, sample(seq(from = 0, to = 10, by = .1), size = 1, replace = TRUE)) # number of sites and rate parameter
+  # }else{
+    # break
+  }
+    dat <- data.frame(p=p)
+    datalist[[i]] <- dat
+    big_data = do.call(rbind,datalist)
+    print(big_data)
+}
+
+# (presence probability, # poisson values, poisson lambda)
+hurdle(4, 5)
+
+
+# 14. Professor Savitzky approaches you with a delicate problem. A member of faculty became disoriented
+# during fieldwork, and is now believed to be randomly wandering somewhere in the desert surrounding
+# Logan. He is modelling their progress through time in five minute intervals, assuming they cover a
+# random, Normally-distributed distance in latitude and longitude in each interval. Could you simulate this
+# process 100 times and plot it for him?
+
+# t = number of 5 min intervals
+
+# average walking speed = 3.1 mph
+# if 3.1 mph then .0833 miles per 5 min on average
+
+progress <- function(t){
+  datalist=list()
+  for(i in 1:t){
+    lat <- (mean(abs(rnorm(100, mean = 0.08333333, sd = 1)))) 
+    lon <- (mean(abs(rnorm(100, mean = 0.08333333, sd = 1)))) 
+    dis <- (sqrt(lat)+sqrt(lon))
+    dat <- data.frame(dis=dis)
+    dat$time <- i*5
+    datalist[[i]] <- dat
+  }
+big_data = do.call(rbind,datalist)
+big_data$dis2 = cumsum(big_data$dis)
+plot(big_data$dis2 ~ big_data$time, xlab='Time (Min)', ylab='Distance Traveled',type='b')
+print(big_data)
+}
+
+progress(10)
+
+ 
+# 15. Professor Savitzky is deeply concerned to realise that the member of faculty was, in fact, at the top of
+# a steep mountain in the fog. Approximately 5 miles away, in all directions, from the faculty member’s
+# starting point is a deadly cliff! He asks if you could run your simulation to see how long, on average,
+# until the faculty member plummets to their doom.
+
+
+# 16. Sadly, by the time you have completed your simulations the faculty member has perished. Professor
+# Savitzky is keen to ensure this will never happen again, and so has suggested each faculty member
+# be attached, via rubber band, to a pole at the centre of the site whenever conducting fieldwork 3. He
+# assures you that you can model this by assuming that the faculty member, at each time-step, moves α×
+# distance-from-pole latitudinally and longitudinally (in addition to the rate of movement you’ve already
+# simulated) each time-step. Simulate this, and see how strong the rubber band (α) must be to keep the
+# faculty member safe for at least a day.
+# 
+
+
+
+# 17. (If you finish early: Most, if not all, faculty members are not as young as they once were. See what effect
+# the faculty member tiring (and eventually sitting down and giving up) would have on their likelihood of
+# survival. What would happen if a faculty member panicked and walked faster through time?
+#                                                                                                                                                   the Poisson distribution). Write a function that simulates the abundance of a species at n sites given a
